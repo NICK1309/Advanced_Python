835fa7c9b49d02330224d8b468eededceee9dc26
Nikita Kamalakar Sali <81757315+NICK1309@users.noreply.github.com>
2023-04-11 19:50:38 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,95 @@
+'''
+import re 
+a = input("Enter string to perform match operation")
+mtch = re.fullmatch(a,"pythonisvery")
+print(mtch)
+if mtch != None:
+    print("match found at begining level")
+    print(mtch.start(), " ",mtch.end())
+else:
+    print("there is no matching at begining level")
+#----------------------------------------------------------------------------------------------------------------
+import re 
+a = input("Enter string to perform match operation")
+mtch = re.search(a,"pythonisssdynamicclann")
+print(mtch)
+if mtch != None:
+    print(mtch.start(), " ",mtch.end())
+else:
+    print("there is no matching at begining level")
+#--------------------------------------------------------------------------------------------------------------    
+    
+import re
+mtch = re.findall('[A-Za-z0-9]',"abc8asd1j3j4")
+print(mtch)
+'''
+#--------------------------------------------------------------------------------------------------------------
+'''
+import re 
+mo=input("Enter Mobile Number.")
+obj=re.fullmatch("[0-9]\d{9}",mo)
+if obj != None:
+    print("Valid Mobile number")
+else:
+    print("Invalid Mobile number")   
+    
+#---------------------------------------------------------------------------------------------------------------
+ Task 01 :- #WAP to accept pw of pw contains uppercase lowercase digit special symbols then print pw is weak or strong.
+import re
+pw = input("Enter a Password.")
+obj = re.search("[A-Za-z0-9\W]",pw)
+if obj != None:
+    print("Password is strong.")
+else:
+    print("Password is weak.")  
+'''
+#--------------------------------------------------------------------------------------------------------------- 
+'''import re
+obj = re.sub("[0-9]","#","asda78asda8ad")
+print(obj)
+'''
+#---------------------------------------------------------
+# stack implementation (Last in first out)
+'''
+import time
+stack = []
+size = int(input("Enter the size of the stack :"))
+for i in range(size):
+    a = int(input("Push the element in stack:"))
+    stack.append(a)
+else:
+    print("Stack is full")
+    print(stack)    
+
+print("Pop operation starts:")
+for i in range(size):
+    time.sleep(2)
+    print(stack.pop()," Pop element from stack. ")
+else:
+    print("Stack is empty")
+    print(stack)   
+    '''
+#---------------------------------------------------------------------------------------------------------     
+# Queue implementation (First in first out)
+'''
+import time
+queue = []
+size = int(input("Enter the size of the queue :"))
+if(size <= 10):
+    for i in range(size):
+        a = int(input("Insert the element in queue:"))
+        queue.append(a)
+    else:
+        print("queue is full")
+        print(queue)    
+
+    print("Removal operation starts:")
+    for i in range(size):
+        time.sleep(2)
+        print(queue.pop(0)," Delete element from Queue. ")
+    else:
+        print("Queue is empty")
+        print(queue)   
+'''
+#---------------------------------------------------------------------------------------------------------------- 
+       
